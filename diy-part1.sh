230f13b8fdfe65d40ea0f65ebb22c4d9de5bd99b
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2022-11-09 23:28:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-sed -i '1i https://github.com/bigbighill/openwrt-packages-1' feeds.conf.default
+sed -i '1i src-git bigbighillhttps://github.com/bigbighill/openwrt-packages-1' feeds.conf.default
 sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
