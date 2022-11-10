6ff091754be3d667be66e50feda9b0bc75a13712
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2022-11-10 19:30:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,6 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '1i src-git themes https://github.com/Lienol/openwrt-luci' feeds.conf.default
 # sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
