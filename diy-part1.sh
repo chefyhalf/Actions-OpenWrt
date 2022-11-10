7f9a9b982aea5c1000b8fe72ef6bd35814e8ed50
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2022-11-10 23:17:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,4 @@
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+git clone https://github.com/kiddin9/openwrt-bypass.git
